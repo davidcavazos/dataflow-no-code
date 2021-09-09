b3aaeca8b68d7a9d1bd301ab6b3a678bb16c44f0
David Cavazos <dcavazos@google.com>
2021-09-09 13:42:18 -0700

    moved functions

--- servers/python/functions/hello.py
+++
@@ -1,5 +0,0 @@
-# User defined function.
-
-
-def hello(element):
-    return f"Hello {element}!"
--- servers/python/functions/with_numbers.py
+++
@@ -1,8 +0,0 @@
-# User defined function.
-
-
-from typing import List
-
-
-def with_numbers(name: str, numbers: List[float]) -> List[str]:
-    return [f"{name} {num}" for num in numbers]
